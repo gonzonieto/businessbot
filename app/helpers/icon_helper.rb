01e0d97a19f79c9397ff3eb3f8517461dfef768b
Gonzo Nieto <gonzonieto@gmail.com>
2023-01-11 04:40:29 -0500

    Add ability to specify additional classes for icons

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,5 +1,5 @@
 module IconHelper
-  def icon name, type
-    content_tag(:i, '', class: "fa-#{type} fa-#{name}")
+  def icon(name, type, classes = "")
+    content_tag(:i, '', class: "fa-#{type} fa-#{name} #{classes}")
   end
 end
